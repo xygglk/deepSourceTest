fe2cbe461e446d47cdba893a8f625d3b4867603c
XYGG <1573034132@qq.com>
2023-02-06 13:15:05 +0800

    代码提交

---
+++ src/main/java/Test2.java
@@ -0,0 +1,33 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author XYGG
+ * @description TODO
+ * @date 2023/2/6 10:56
+ */
+public class Test2 {
+    public void Test1() {
+        List list = new ArrayList<>();
+        getList(list);
+
+    }
+
+    public static void getList(List list) {
+        while (true) {
+            System.out.println(list.get(0));
+        }
+    }
+
+    public void empty() {
+
+    }
+
+    public void     format(int x, int y) {
+         int a =     0;
+
+    }
+
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author XYGG
+ * @description TODO
+ * @date 2023/2/6 10:56
+ */
+public class Test2 {
+    @Test
+    public void Test1() {
+        List list = new ArrayList<>();
+        getList(list);
+
+    }
+
+    public static void getList(List list) {
+        while (true) {
+            System.out.println(list.get(0));
+        }
+    }
+
+    public void empty() {
+
+    }
+
+    public void     format(int x, int y) {
+         int a =     0;
+
+    }
+
+}
