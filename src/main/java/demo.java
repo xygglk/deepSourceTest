eac9c86fb35a38fbfe5a2bcc27652ed92c9275c1
XYGG <1573034132@qq.com>
2023-02-06 13:50:32 +0800

    代码提交3

---
+++ src/main/java/demo.java
@@ -0,0 +1,19 @@
+
+import  com.jogamp.common.util.*;
+import netscape.javascript.JSObject;
+public class demo {
+    int a= 0;
+    String name;//名字
+    String description;//描述
+    String age;//年龄
+    String address;//地址
+
+   private String name(String   name){
+       return name+a;
+   }
+   public void getName(){
+       return ;
+   }
+
+
+}
